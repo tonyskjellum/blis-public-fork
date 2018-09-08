1d7ea83f3208f812ae89af7c5ef94136423c78ea
Tony Skjellum <skjellum@gmail.com>
2018-09-08 15:46:12 -0400

    A little more work; stopping for today.

--- kernels/armv8a/1/bli_dotv_armv8a_int.c
+++ kernels/armv8a/1/bli_dotv_armv8a_int.c
@@ -249,8 +249,11 @@ void bli_sdotv_armv8a_int
 	   [_nv_iter] "m" (n_viter) // 2
 
 	 : // Register clobber list
-	   "x0", "x1", "x8",
-           "v28", "v29", "v30", "v31"  // more coming soon 
+	   "x0", "x1", "x8", "x9"
+           "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7",
+	 //"v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15",
+	   "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23",
+	   "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31"  
         );
 
 
